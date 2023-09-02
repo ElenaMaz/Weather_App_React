3024a319335de67ca6f76a95f69c5afc76ebc484
Elena <helena151612@gmail.com>
2023-09-02 19:56:43 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  <title>Weather app</title>;
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <div className="header">
+        <h1>Weather app</h1>
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          onChange={updateCity}
+        />
+        <button type="Submit">Search</button>
+      </div>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div className="App">
+        {form}
+        <div className="container">
+          <p>City: {city}</p>
+          <ul>
+            <li>Temperature: {Math.round(weather.temperature)}°C</li>
+            <li>Description: {weather.description}</li>
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind: {weather.wind}km/h</li>
+            <li>
+              <img src={weather.icon} alt={weather.description} />
+            </li>
+          </ul>
+        </div>
+        <footer> Coded by Olena Mazurenko</footer>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
